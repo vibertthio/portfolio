271a8479e0b36d3a9ef9d86164b55d4e43c982d9
Vibert Thio <vibertthio@gmail.com>
2017-11-13 03:01:43 +0800

    :construction: sticky navBar

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -6,6 +6,29 @@ import NavList from './nav-list';
 import signBlack from '../../assets/images/sign-black.png';
 import logo from '../../assets/images/logo.png';
 
+const NabBar = props => (
+  <header
+    className={`${props.sticky > 0 ? styles.fixedHeader : ''}`}
+    style={{ marginBottom: '1.5rem' }}
+  >
+    <div className={`${props.sticky > 0 ? styles.fixedNavBar : ''}`}>
+      <Link
+        className={styles.logoLink}
+        to="/"
+        style={{ textShadow: 'none', backgroundImage: 'none' }}
+      >
+        <img className={styles.flashLogo} src={logo} alt="logo" />
+        <div className={styles.navLogo}>
+          <h3>{props.site.siteMetadata.title}</h3>
+        </div>
+        <img className={styles.signBlack} src={signBlack} alt="sign-black" />
+      </Link>
+      <NavList />
+      <Collapse />
+    </div>
+  </header>
+);
+
 class Layout extends Component {
 	constructor() {
 		super();
@@ -15,38 +38,20 @@ class Layout extends Component {
 	}
 
 	componentDidMount() {
-		// window.addEventListener('scroll', () => {
-		//   console.log('scroll down!');
-		// 	this.setState({
-		// 		scrolledDown: true,
-		// 	});
-		// });
+		window.addEventListener('scroll', () => {
+			console.log('scroll down!');
+			this.setState({
+				scrolledDown: true,
+			});
+		});
 	}
 
 	render() {
 		return (
-  <div
-    className={`${styles.layout} ${this.state.scrolledDown ? '' : styles.scrolled}`}
-    style={{ margin: '0 auto', maxWidth: 1100, padding: '1.25rem 1.0rem' }}
-  >
-    <header style={{ marginBottom: '1.5rem' }}>
-      <Link
-        className={styles.logoLink}
-        to="/"
-        style={{ textShadow: 'none', backgroundImage: 'none' }}
-      >
-        <img className={styles.flashLogo} src={logo} alt="logo" />
-        <div className={styles.navLogo}>
-          <h3>{this.props.data.site.siteMetadata.title}</h3>
-        </div>
-        <img className={styles.signBlack} src={signBlack} alt="sign-black" />
-      </Link>
-      <NavList />
-      <Collapse />
-    </header>
-    <div className={styles.body}>
-      {this.props.children()}
-    </div>
+  <div className={`${styles.layout} ${this.state.scrolledDown ? '' : styles.scrolled}`}>
+    <NabBar site={this.props.data.site} sticky={false} />
+    <NabBar site={this.props.data.site} sticky />
+    <div className={styles.body}>{this.props.children()}</div>
     <footer>
       <div className={styles.footerContainer}>
         <h5 className={styles.footerContent}>2017 @ Vibert Thio</h5>
