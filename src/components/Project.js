7531e6ac6d10a5ecc18240653a3af5ed959e6934
Vibert Thio <vibertthio@gmail.com>
2017-11-13 14:22:14 +0800

    :lipstick: add image loader

--- src/components/Project.js
+++ src/components/Project.js
@@ -2,24 +2,47 @@ import React from 'react';
 import styles from '../pages/projects.module.css';
 import FadeIn from '../utils/fade-in';
 
-const Project = props => (
+class Project extends React.Component {
+	constructor() {
+		super();
+		this.state = {
+			loadingImg: true,
+		};
+	}
+
+	handleFinishLoadingImg() {
+    this.setState({
+      loadingImg: false,
+    });
+  }
+
+	render() {
+		return (
   <div className={styles.projectContainer}>
     <FadeIn>
       <div>
-        <h1 className={styles.title}>{props.title}</h1>
+        <h1 className={styles.title}>{this.props.title}</h1>
         <div
           className={`
-            ${styles.projectImgContainer}
-            ${props.index % 2 === 0 ? styles.left : styles.right}`}
+                ${styles.projectImgContainer}
+                ${this.props.index % 2 === 0 ? styles.left : styles.right}
+                ${this.state.loadingImg ? styles.loading : ''}`}
         >
-          <img src={props.img} className={styles.projectImg} alt="project-img" />
+          <img
+            src={this.props.img}
+            className={styles.projectImg}
+            alt="project-img"
+            onLoad={() => this.handleFinishLoadingImg()}
+          />
         </div>
         <div className={styles.descriptionContainer}>
-          <p className={styles.description}>{props.content}</p>
+          <p className={styles.description}>{this.props.content}</p>
         </div>
       </div>
     </FadeIn>
   </div>
-);
+		);
+	}
+}
 
 export default Project;
