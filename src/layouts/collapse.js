fea76120dcb3efee530058a24a7b9566b55a132e
Vibert Thio <vibertthio@gmail.com>
2017-11-13 16:41:37 +0800

    :fire: remove close menu button

--- src/layouts/collapse.js
+++ src/layouts/collapse.js
@@ -2,14 +2,17 @@ import React, { Component } from 'react';
 import Link, { withPrefix } from 'gatsby-link';
 import FadeIn from '../utils/fade-in';
 
-import styles from './index.module.css';
+import styles from './collapse.module.css';
+import layoutStyles from './index.module.css';
 import menuIcon from '../../assets/images/menu-icon.png';
 import closeIcon from '../../assets/images/cross-icon.png';
 import signWhite from '../../assets/images/sign-white.png';
 
 const ListLink = props => (
   <Link
-    className={`${styles.link} ${withPrefix(props.to) === props.path ? styles.active : styles.inactive}`}
+    className={`${styles.link} ${
+			withPrefix(props.to) === props.path ? styles.active : styles.inactive
+		}`}
     onClick={() => props.onClick()}
     to={props.to}
   >
@@ -52,27 +55,31 @@ class Collapse extends Component {
 		return (
   <div style={{ float: 'right' }}>
     <div id="menu" className={styles.overlay}>
-      <button
-        href=""
-        className={`${styles.closeBtn} ${styles.btn}`}
-        onClick={() => this.closeMenu()}
-      >
-        <img className={styles.closeIcon} src={closeIcon} alt="close-icon" />
-      </button>
-      <img className={styles.signWhite} src={signWhite} alt="sign-black" />
-      <div className={styles.overlayContent}>
-        <ListLink to="/" path={this.props.path} onClick={() => this.closeMenu()}>
-							Home
-        </ListLink>
-        <ListLink to="/projects/" path={this.props.path} onClick={() => this.closeMenu()}>
-							Projects
-        </ListLink>
-        <ListLink to="/blogs/" path={this.props.path} onClick={() => this.closeMenu()}>
-							Blogs
-        </ListLink>
+      <div className={styles.menuContainer}>
+        <img className={styles.signWhite} src={signWhite} alt="sign-black" />
+        <div className={styles.overlayContent}>
+          <ListLink to="/" path={this.props.path} onClick={() => this.closeMenu()}>
+								Home
+          </ListLink>
+          <ListLink to="/projects/" path={this.props.path} onClick={() => this.closeMenu()}>
+								Projects
+          </ListLink>
+          <ListLink to="/blogs/" path={this.props.path} onClick={() => this.closeMenu()}>
+								Blogs
+          </ListLink>
+          {this.state.open && (
+          <button
+            href=""
+            className={`${styles.closeBtn} ${styles.btn}`}
+            onClick={() => this.closeMenu()}
+          >
+            {/* <img className={styles.closeIcon} src={closeIcon} alt="close-icon" /> */}
+          </button>
+							)}
+        </div>
       </div>
     </div>
-    <button className={`${styles.btn} ${styles.menuBtn}`} onClick={() => this.onClick()}>
+    <button className={`${styles.btn} ${layoutStyles.menuBtn}`} onClick={() => this.onClick()}>
       <FadeIn
         right
         opacity={{
@@ -88,7 +95,7 @@ class Collapse extends Component {
 							damping: 15,
 						}}
       >
-        <img className={styles.menuIcon} src={menuIcon} alt="menu-icon" />
+        <img className={layoutStyles.menuIcon} src={menuIcon} alt="menu-icon" />
       </FadeIn>
     </button>
   </div>
