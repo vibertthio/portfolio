3e27efdbf28ebf3c22b8c169f78103465b3dce5d
Vibert Thio <vibertthio@gmail.com>
2017-11-15 14:47:14 +0800

    :book: revise contact

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default () => (
   <div>
-    <h1>I would love to talk! Email me at the address below</h1>
+    <h1>Contact</h1>
     <p>
       <a href="mailto:vibertthio@gmail.com">vibertthio at gmail</a>
     </p>
